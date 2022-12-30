0f655072e20858881b19a8c4e5d79c7e931c4a93
hiimKhanh26 <luuquockhanh2603@gmail.com>
2022-12-30 15:27:39 +0700

    fix width 3

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="body">
       {show && (
-        <div className="container">
+        <div className="content">
           <div className="left">
             <SearchCity />
 
